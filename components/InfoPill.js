32efbc200b665cf63d2d17eb0d6d1ba037329464
Shaurya Jain <shaurya26.jain2@gmail.com>
2021-04-28 19:35:09 +0530

    Info Pills added

---
+++ components/InfoPill.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {View} from 'react-native';
+import {scale, verticalScale} from 'react-native-size-matters';
+import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
+import {infoIconNames} from '../constants/infoIconsConfig';
+import styles from '../styles';
+import AppText from './AppText';
+
+const InfoPill = ({
+  text,
+  iconName,
+  iconType,
+  backgroundColor,
+  contentColor,
+  ...props
+}) => {
+  return (
+    <View
+      style={{
+        height: verticalScale(20),
+        backgroundColor: backgroundColor ?? 'white',
+        paddingHorizontal: scale(10),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        margin: scale(2.5),
+        borderRadius: scale(100),
+      }}>
+      <View
+        style={{
+          width: scale(10),
+          ...styles.centerView,
+        }}>
+        <FontAwesome5Icon
+          name={infoIconNames[iconType] ?? iconName}
+          color={contentColor ?? 'black'}
+        />
+      </View>
+      <AppText
+        style={{
+          fontSize: verticalScale(10),
+          color: contentColor ?? 'black',
+          marginLeft: scale(5),
+          alignSelf: 'center',
+        }}>
+        {text}
+      </AppText>
+    </View>
+  );
+};
+
+export default InfoPill;
---
+++ constants/infoIconsConfig.js
@@ -0,0 +1,38 @@
+import colors from './colors';
+
+//Font Awesome 5 Icon names corresponding to info type
+export const infoIconNames = {
+  Zodiac: 'moon',
+  Drinks: 'wine-glass-alt',
+  Smokes: 'smoking',
+  'Looking For': 'search',
+  Political: 'balance-scale',
+  Food: 'utensils',
+};
+
+export const infoIconColors = {
+  Zodiac: {
+    backgroundColor: colors.primary,
+    contentColor: 'white',
+  },
+  Drinks: {
+    backgroundColor: colors.primary,
+    contentColor: 'white',
+  },
+  Smokes: {
+    backgroundColor: colors.primary,
+    contentColor: 'white',
+  },
+  'Looking For': {
+    backgroundColor: colors.primary,
+    contentColor: 'white',
+  },
+  Political: {
+    backgroundColor: colors.primary,
+    contentColor: 'white',
+  },
+  Food: {
+    backgroundColor: colors.primary,
+    contentColor: 'white',
+  },
+};
