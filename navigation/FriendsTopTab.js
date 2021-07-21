ee038ca006e67fb35e3d4b8ca77288da660b761d
Shaurya Jain <shaurya26.jain2@gmail.com>
2021-07-21 18:17:46 +0530

    label font size

--- navigation/FriendsTopTab.js
+++ navigation/FriendsTopTab.js
@@ -1,23 +1,26 @@
-import React from "react";
-import { View } from "react-native";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import SearchFriendsScreen from "../screens/friends/SearchFriendsScreen";
-import {
-  SafeAreaView,
-  useSafeAreaInsets,
-} from "react-native-safe-area-context";
-import FriendRequestsScreen from "../screens/friends/FriendRequestsScreen";
-import FriendsScreen from "../screens/friends/FriendsScreen";
-import CustomSafeAreaView from "../components/CustomSafeAreaView";
-import styles from "../styles";
-import StreamsListScreen from "../screens/streams/StreamsListScreen";
+import React from 'react';
+import {View} from 'react-native';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+import SearchFriendsScreen from '../screens/friends/SearchFriendsScreen';
+import {SafeAreaView, useSafeAreaInsets} from 'react-native-safe-area-context';
+import FriendRequestsScreen from '../screens/friends/FriendRequestsScreen';
+import FriendsScreen from '../screens/friends/FriendsScreen';
+import CustomSafeAreaView from '../components/CustomSafeAreaView';
+import styles from '../styles';
+import StreamsListScreen from '../screens/streams/StreamsListScreen';
+import { scale } from 'react-native-size-matters';
 
 const FriendsTopTabNavigator = createMaterialTopTabNavigator();
 
 export default FriendsTopTab = () => {
   return (
     <CustomSafeAreaView style={styles.rootView}>
-      <FriendsTopTabNavigator.Navigator>
+      <FriendsTopTabNavigator.Navigator
+        tabBarOptions={{
+          labelStyle: {
+            fontSize: scale(10),
+          },
+        }}>
         <FriendsTopTabNavigator.Screen
           name="Friends"
           component={FriendsScreen}
